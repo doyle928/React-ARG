e2d271825e57dc105a2635aff2c873df14f11e6b
doyle928 <doyle928@yahoo.com>
2019-06-27 13:21:01 -0400

    fixed feather icons

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-$(".home-container-card").hover(
-    function() {
-        console.log("working");
-        $( this ).find("h2").addClass( "underline" );
-    }, function() {
-      $( this ).find("h2").removeClass( "underline" );
-    }
-);
-$(".home-container-card").off( "mouseenter mouseleave" );
