7e3bf7b2b0b9932800892f20e59d02f1953bb172
Matt Doyle <35369419+doyle928@users.noreply.github.com>
2019-07-05 22:21:40 -0400

    Update HomeSplash.js

--- src/components/pages/home-components/HomeSplash.js
+++ src/components/pages/home-components/HomeSplash.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../../stylesheets/home-splash.css";
-// import GoogleAuth from "./GoogleAuth";
 
 const HomeSplash = () => {
   return (
