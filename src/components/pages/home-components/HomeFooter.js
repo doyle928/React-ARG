0c30b6ebc255d85ee140248b350057ea1653042f
Matt Doyle <35369419+doyle928@users.noreply.github.com>
2019-07-05 22:21:29 -0400

    Update HomeFooter.js

--- src/components/pages/home-components/HomeFooter.js
+++ src/components/pages/home-components/HomeFooter.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../../stylesheets/home-footer.css";
-// import GoogleAuth from "./GoogleAuth";
 
 const HomeFooter = () => {
   return (
